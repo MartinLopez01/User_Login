440fe32b402f37ab14ca71227117a6adb5de1148
Martin Lopez <90067541+MartinLopez01@users.noreply.github.com>
2023-03-25 14:59:23 -0600

    Add files via upload

---
+++ User_Login/src/Ayuda.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Ayuda extends JFrame{
+	private static final long serialVersionUID = 1L;
+
+	public Ayuda() {
+		this.setVisible(true);
+		this.setSize(800, 700);
+		this.setTitle("Accede a tu cuenta");
+		this.setResizable(true);
+		this.getContentPane().setBackground(new Color(28,113,198));
+		this.setLayout(null);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+		JPanel p1 = new JPanel();
+		p1.setSize(getWidth(), getHeight());
+		p1.setBackground(Color.DARK_GRAY);
+		this.add(p1);
+			
+	}
+
+}
---
+++ User_Login/src/Opciones.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Opciones extends JFrame{
+
+	private static final long serialVersionUID = 1L;
+	
+	public Opciones() {
+		this.setVisible(true);
+		this.setSize(800, 700);
+		this.setTitle("Accede a tu cuenta");
+		this.setResizable(true);
+		this.getContentPane().setBackground(new Color(28,113,198));
+		this.setLayout(null);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+		JPanel p1 = new JPanel();
+		p1.setSize(getWidth(), getHeight());
+		p1.setBackground(Color.CYAN);
+		this.add(p1);
+		
+	}
+
+}
